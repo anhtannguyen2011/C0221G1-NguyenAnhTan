03229a79a7f900e36259da93d3a8ce4c928c17fa
nguyen_anh_tan <anhtannguyen2011@gmail.com>
2021-05-17 22:52:40 +0700

    Sửa lỗi bài tập 4

--- module_3/04_db_use_function/exercise/exercise_01.sql
+++ module_3/04_db_use_function/exercise/exercise_01.sql
@@ -8,19 +8,13 @@ having s.credit = (select max(credit) from `subject`);
 
 -- hiển thị thông tin môn học ó điểm thi cao nhất
 
-select s.*, m.mark as "Điểm Cao Nhất"  from `subject` as s 
+select s.sub_name , s.sub_id , s.credit
+from `subject` as s
 join mark as m on m.sub_id = s.sub_id
-group by s.sub_id,s.sub_name
-having m.mark >= all(select max(mark) from mark group by mark.mark_id);
+where m.mark >= all(select max(mark) from mark);
 
 
 
--- hiển thị tất cả các môn học có điểm thi cao nhất
--- select s.sub_name,s.credit,s.`Status`, max(m.mark) as "Điểm Cao Nhất"
--- from `subject` as s 
--- join mark as m on s.sub_id = m.sub_id
--- group by s.sub_id,s.sub_name
--- order by max(m.mark) DESC;
 
 insert into mark
 values (4,3,3,7,1);
@@ -31,6 +25,6 @@ where s.student_id = 2;
 
 select s.student_name,s.address,s.phone , avg(m.mark) as "Điểm Trung Bình"
 from student as s 
-join mark as m on s.student_id = m.student_id
+left join mark as m on s.student_id = m.student_id
 group by s.student_id,s.student_name
 order by avg(m.mark) DESC;

e2075bf8d4b96565c161f17a07b9a932df338abb
nguyen_anh_tan <anhtannguyen2011@gmail.com>
2021-06-21 16:16:58 +0700

    update bài tập và thực hành

---
+++ module_4/06_JPA/thuc_hanh/customer-management/src/main/java/com/codegym/model/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.codegym.model.repository;
+
+import com.codegym.model.entity.Customer;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
---
+++ module_4/06_JPA/thuc_hanh/customer-management/src/main/java/com/codegym/model/service/Impl/CustomerServiceImpl.java
@@ -0,0 +1,32 @@
+package com.codegym.model.service.Impl;
+
+import com.codegym.model.entity.Customer;
+import com.codegym.model.repository.ICustomerRepository;
+import com.codegym.model.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+public class CustomerServiceImpl implements ICustomerService {
+    @Autowired
+    ICustomerRepository customerRepository;
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.remove(id);
+    }
+}

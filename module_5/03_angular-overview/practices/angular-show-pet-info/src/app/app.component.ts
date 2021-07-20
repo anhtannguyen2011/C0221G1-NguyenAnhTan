8a9e094e2bbfeed907bd098429e4c0c8430fd398
nguyen_anh_tan <anhtannguyen2011@gmail.com>
2021-07-20 09:08:14 +0700

    update

---
+++ module_5/03_angular-overview/practices/angular-show-pet-info/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {Pet} from "./pet";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-show-pet-info';
+  pet: Pet = {
+    name: 'puppie',
+    image: 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg'
+  };
+}
